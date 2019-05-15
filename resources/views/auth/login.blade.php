1852d05d9ec8a1f6ebf23b0e4efbcdd26ad6e338
Erfan-PC <erfantkerfan@yahoo.com>
2019-05-16 02:51:32 +0430

    added recaptcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
                             <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
                                 <label class="col-md-4 justify-content-center control-label">ربات نیستم</label>
                                 <div class="col-md-6 pull-center">
-                                    {!! app('captcha')->display() !!}
+                                    <div data-sitekey="{{env('NOCAPTCHA_SECRET')}}-84TK-vaUEZ-3" class="g-recaptcha"><div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LdYxqMUAAAAAFlnvIZWJ3vdbYZ-84TK-vaUEZ-3&amp;co=aHR0cHM6Ly9oc3Nob2hhZGEuY29tOjQ0Mw..&amp;hl=en&amp;v=v1555968629716&amp;size=normal&amp;cb=xvm23ufjtrv9" width="304" height="78" role="presentation" name="a-7zg7vudv2j50" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div></div>
                                     @if ($errors->has('g-recaptcha-response'))
                                         <span class="help-block">
                                             <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
