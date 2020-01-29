04bf7a1cdb6939c277e00a2b0ee5777bd28070b5
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-01-29 17:58:07 +0330

    fix cwebp helpr

--- app/helpers.php
+++ app/helpers.php
@@ -7,9 +7,9 @@ function cwebp($in , $out){
         $command = "cwebp -quiet -mt -m 6 -q 80 -sharp_yuv -alpha_filter best -pass 10 -segments 4 -af ".$in." -o ".$out.".webp";
         $process = new Process([$command]);
         $process->run();
-        return 1;
-    } catch (Exception $e){
-        return 'Caught exception: '.$e->getMessage();
+        $x = 1;
+    } catch (Exception $e) {
+        $x = 'Caught exception: ' . $e->getMessage();
     }
-
+    return $x;
 }
