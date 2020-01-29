be704cc70ff2ef00623895aa01e77d63c2f518ab
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-01-29 18:19:39 +0330

    fix cwebp use shell_exec instead of prcs

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,9 @@
 <?php
 
-use Symfony\Component\Process\Process;
-
 function cwebp($in , $out){
     try {
         $command = "cwebp -quiet -mt -m 6 -q 80 -sharp_yuv -alpha_filter best -pass 10 -segments 4 -af ".$in." -o ".$out.".webp";
-        $process = new Process([$command]);
-        $process->run();
+        shell_exec($command);
         $x = 1;
     } catch (Exception $e) {
         $x = 'Caught exception: ' . $e->getMessage();
