7b21db005815947016279012986e6c979978012e
erfantkerfan@yahoo.com <erfantkerfan@yahoo.com>
2020-02-02 15:33:33 +0330

    fix last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,40 +102,36 @@
 
         {{-- BANNER --}}
         <div class="col-md-7">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                @foreach($galleries as $gallery)
-                    <li data-target="#myCarousel2" data-slide-to="{{$gallery->id}}"></li>
-                @endforeach
-            </ol>
+            <div id="myCarousel2" data-interval="2500" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    @foreach($galleries as $gallery)
+                        <li data-target="#myCarousel2" data-slide-to="{{$gallery->id}}"></li>
+                    @endforeach
+                </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                @foreach($galleries as $gallery)
-                    <div class="item {{ $loop->first ? ' active' : '' }}">
-                        <a href="{{route('news_single',['type'=>$gallery->type,'id'=>$gallery->id])}}">
-                            <img src="/img/news/{{$gallery->id}}/1.webp" style="width:100%;">
-                        </a>
-                        {{--<div class="carousel-caption d-none d-md-block">--}}
-                        {{--<a href="{{route('news_single',['id'=>$new->id])}}" style="text-decoration: none;color: yellow;font-size: 15px">--}}
-                        {{--مشاهده--}}
-                        {{--{{$new->title}}--}}
-                        {{--</a>--}}
-                        {{--</div>--}}
-                    </div>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    @foreach($galleries as $gallery)
+                        <div class="item {{ $loop->first ? ' active' : '' }}">
+                            <a href="{{route('news_single',['type'=>$gallery->type,'id'=>$gallery->id])}}">
+                                <img src="/img/news/{{$gallery->id}}/1.webp" style="width:100%;">
+                            </a>
+                        </div>
 
-                @endforeach
+                    @endforeach
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">قبلی</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel2" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">بعدی</span>
+                </a>
             </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">قبلی</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel2" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">بعدی</span>
-            </a>
         </div>
 
         <div class="col-md-3">
@@ -172,40 +168,38 @@
             </div>
 
             {{-- GALLERY --}}
-            <div id="myCarousel2" data-interval="2500" class="carousel slide" data-ride="carousel">
-                <div id="myCarousel" data-interval="4000" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        @foreach($sliders as $slider)
-                            <li data-target="#myCarousel" data-slide-to="{{$slider->id}}"></li>
-                        @endforeach
-                    </ol>
+            <div id="myCarousel" data-interval="4000" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    @foreach($sliders as $slider)
+                        <li data-target="#myCarousel" data-slide-to="{{$slider->id}}"></li>
+                    @endforeach
+                </ol>
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        @foreach($sliders as $slider)
-                            <div class="item {{ $loop->first ? ' active' : '' }}">
-                                <img src="/img/slider/{{$slider->id}}.webp" style="width:100%;">
-                                <div class="carousel-caption d-md-block">
-                                    <h5 style="mix-blend-mode: difference">{{$slider->head}}</h5>
-                                    <p style="mix-blend-mode: difference">{{$slider->body}}</p>
-                                </div>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    @foreach($sliders as $slider)
+                        <div class="item {{ $loop->first ? ' active' : '' }}">
+                            <img src="/img/slider/{{$slider->id}}.webp" style="width:100%;">
+                            <div class="carousel-caption d-md-block">
+                                <h5 style="mix-blend-mode: difference">{{$slider->head}}</h5>
+                                <p style="mix-blend-mode: difference">{{$slider->body}}</p>
                             </div>
-                        @endforeach
-                    </div>
-
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                        <span class="sr-only">قبلی</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                        <span class="sr-only">بعدی</span>
-                    </a>
+                        </div>
+                    @endforeach
                 </div>
 
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">قبلی</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">بعدی</span>
+                </a>
             </div>
+
         </div>
 
     </div>
