43590d69762b3d6456ba6ad93154279dd87eca4f
erfantkerfan@yahoo.com <erfantkerfan@yahoo.com>
2020-02-05 10:35:39 +0330

    change home gallery size

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -10,7 +10,7 @@ class StaticController extends Controller
     public function home()
     {
         $sliders = Slider::all();
-        $galleries = News::whereBetween('type', array(300, 399))->where('files', '!=', '0')->take(5)->orderBy('created_at', 'desc')->get();
+        $galleries = News::whereBetween('type', array(300, 399))->where('files', '!=', '0')->take(10)->orderBy('created_at', 'desc')->get();
         $news = News::whereBetween('type', array(300, 399))->orderBy('date', 'desc')->take(5)->get();
         return view('home')->with(['sliders' => $sliders, 'galleries' => $galleries, 'news' => $news]);
     }
