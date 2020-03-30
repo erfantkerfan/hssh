a8818449433bb5d78f79157644298c7db9eab14c
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-03-30 21:57:43 +0430

    video page dir pull right

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -2,34 +2,32 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <br>
-                <br>
-                <br>
-                <div class="panel panel-warning">
-                    <div class="panel-heading text-center">
-                        فایل های ویدئویی
-                    </div>
-                    <div class="panel-body" dir="rtl">
-                        @foreach($videos as $chunk)
-                            <div class="row">
-                                @foreach($chunk as $video)
-                                    <div class="col-md-4 mx-auto">
-                                        <div class="text-center">
-                                            {{$video->title}}
-                                        </div>
-                                        <video controls width="100%" height="100%" preload="metadata">
-                                            <source src="{{'/video/'.$video->file}}" type="video/mp4">
-                                            Your browser does not support the video tag.
-                                        </video>
+        <div class="col-md-12">
+            <br>
+            <br>
+            <br>
+            <div class="panel panel-warning">
+                <div class="panel-heading text-center">
+                    فایل های ویدئویی
+                </div>
+                <div class="panel-body">
+                    @foreach($videos as $chunk)
+                        <div class="row">
+                            @foreach($chunk as $video)
+                                <div class="col-md-4 mx-auto pull-right">
+                                    <div class="text-center">
+                                        {{$video->title}}
                                     </div>
-                                @endforeach
-                            </div>
-                            <br>
-                            <br>
-                        @endforeach
-                    </div>
+                                    <video controls width="100%" height="100%" preload="metadata">
+                                        <source src="{{'/video/'.$video->file}}" type="video/mp4">
+                                        Your browser does not support the video tag.
+                                    </video>
+                                </div>
+                            @endforeach
+                        </div>
+                        <br>
+                        <br>
+                    @endforeach
                 </div>
             </div>
         </div>
